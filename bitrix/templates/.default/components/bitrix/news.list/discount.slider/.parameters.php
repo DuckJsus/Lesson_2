e3f04f53cb32d8ab1b2f8ff6e2d329a96a8204a3
ASDerevyanko <ASDerevyanko@1cbit.ru>
2020-11-18 14:25:18 +0300

    + элементы для размеров изображения на лету

---
+++ bitrix/templates/.default/components/bitrix/news.list/discount.slider/.parameters.php
@@ -0,0 +1,37 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arTemplateParameters = array(
+	"DISPLAY_DATE" => Array(
+		"NAME" => GetMessage("T_IBLOCK_DESC_NEWS_DATE"),
+		"TYPE" => "CHECKBOX",
+		"DEFAULT" => "Y",
+	),
+	"DISPLAY_NAME" => Array(
+		"NAME" => GetMessage("T_IBLOCK_DESC_NEWS_NAME"),
+		"TYPE" => "CHECKBOX",
+		"DEFAULT" => "Y",
+	),
+	"DISPLAY_PICTURE" => Array(
+		"NAME" => GetMessage("T_IBLOCK_DESC_NEWS_PICTURE"),
+		"TYPE" => "CHECKBOX",
+		"DEFAULT" => "Y",
+	),
+	"DISPLAY_PREVIEW_TEXT" => Array(
+		"NAME" => GetMessage("T_IBLOCK_DESC_NEWS_TEXT"),
+		"TYPE" => "CHECKBOX",
+		"DEFAULT" => "Y",
+	),
+);
+
+$arTemplateParameters["LIST_PREV_PICT_H"] = array(
+	"NAME" => GetMessage("LIST_PREV_PICT_H"),
+		"TYPE" => "STRING",
+		"DEFAULT" => "100",
+);
+$arTemplateParameters["LIST_PREV_PICT_W"] = array(
+	"NAME" => GetMessage("LIST_PREV_PICT_W"),
+		"TYPE" => "STRING",
+		"DEFAULT" => "150",
+);
+?>
