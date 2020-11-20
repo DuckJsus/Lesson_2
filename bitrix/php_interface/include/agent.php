c7b1ed51d95765ae12817e17c3449cf37c27dcb7
ASDerevyanko <ASDerevyanko@1cbit.ru>
2020-11-20 13:36:21 +0300

    +комментарии

--- bitrix/php_interface/include/agent.php
+++ bitrix/php_interface/include/agent.php
@@ -1,17 +1,19 @@
 <?
+//Функция проверяет наличие акций с истекшим сроком и отправляет сообщение на email всем, кто в группе Администраторы
 function AgentCheckCurrency()
 {
     if(CModule::IncludeModule("iblock"))
-    {
+    {   
+        //Получаем переменные из инфоблока
         $arSelect = Array("ID", "NAME");
         $arFilter = Array("DISCOUNT_IBLOCK_ID"=>4, "!ACTIVE_DATE"=>"Y", "ACTIVE"=>"Y");
         $res = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>50), $arSelect);
-        
+        //
         while($ob = $res->GetNextElement())
         {
             $arItems[] = $ob->GetFields();
         }
-    
+        //Делает запись в лог
         CEventLog::Add(array(
             "SEVERITY" => "SECURITY",
             "AUDIT_TYPE_ID" => "CHECK_DISCOUNT",
@@ -20,6 +22,7 @@ function AgentCheckCurrency()
             "DESCRIPTION" => "Проверка актуальности, устарели ".count($arItems)." элемента",
         ));
     
+        //Проверяем наличие акций. Отправляем письма, если есть просроченные акции
         if(count($arItems)>0)
         {
             $filter = array("GROUP_ADMIN_ID" => array(1));
