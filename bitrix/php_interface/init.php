63bc6777f3c9e25ed4f608c9cd694aa7cb3079ab
ASDerevyanko <ASDerevyanko@1cbit.ru>
2020-11-20 13:16:15 +0300

    +файл с агентом оповещения о просроченных акциях

---
+++ bitrix/php_interface/init.php
@@ -0,0 +1,11 @@
+<?
+if (file_exists($_SERVER["DOCUMENT_ROOT"]."/bitrix/php_interface/include/constants.php"))
+	require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/php_interface/include/constants.php");
+
+if (file_exists($_SERVER["DOCUMENT_ROOT"]."/bitrix/php_interface/include/functions.php"))
+	require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/php_interface/include/functions.php");
+	
+if (file_exists($_SERVER["DOCUMENT_ROOT"]."/bitrix/php_interface/include/agent.php"))
+	require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/php_interface/include/agent.php");
+?>
+
