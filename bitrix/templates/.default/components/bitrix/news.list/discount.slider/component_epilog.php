a1fddff65e71d33b405e47f6c7f4b92bdee3bf19
ASDerevyanko <ASDerevyanko@1cbit.ru>
2020-11-27 11:32:57 +0300

    Перенес скрипты в папку js

--- bitrix/templates/.default/components/bitrix/news.list/discount.slider/component_epilog.php
+++ bitrix/templates/.default/components/bitrix/news.list/discount.slider/component_epilog.php
@@ -1,6 +1,7 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
     use Bitrix\Main\Page\Asset;
-    Asset::getInstance()->addJs("/bitrix/templates/.default/components/bitrix/news.list/discount.slider/jquery-1.8.2.min.js");
-    Asset::getInstance()->addJs("/bitrix/templates/.default/components/bitrix/news.list/discount.slider/slides.min.jquery.js");
+    Asset::getInstance()->addJs("/bitrix/templates/.default/components/bitrix/news.list/discount.slider/js/jquery-1.8.2.min.js");
+    Asset::getInstance()->addJs("/bitrix/templates/.default/components/bitrix/news.list/discount.slider/js/slides.min.jquery.js");
+    Asset::getInstance()->addJs("/bitrix/templates/.default/components/bitrix/news.list/discount.slider/js/functions.js");
     Asset::getInstance()->addCss("/bitrix/templates/.default/components/bitrix/news.list/discount.slider/style.css");
 ?>
