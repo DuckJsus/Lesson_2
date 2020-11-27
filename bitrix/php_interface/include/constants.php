7204325be5ff2e48efa82861fe5232651ba82c74
ASDerevyanko <ASDerevyanko@1cbit.ru>
2020-11-27 11:35:33 +0300

    +константа инфоблока Вакансии

--- bitrix/php_interface/include/constants.php
+++ bitrix/php_interface/include/constants.php
@@ -5,6 +5,8 @@ define('CATALOG_IBLOCK_ID',1);
 define('NEWS_IBLOCK_ID',2);
 //Инфоблок Акции_sales
 define('DISCOUNT_IBLOCK_ID',4);
+//Инфоблок Вакансии
+define('VACANCY_IBLOCK_ID',5);
 
 //ID группы "Администраторы"
 define('GROUP_ADMIN_ID',1);
