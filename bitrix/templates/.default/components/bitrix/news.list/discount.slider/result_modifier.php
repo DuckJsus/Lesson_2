c8f58457d25a825533b2bf8efcc700ce45d7c5eb
ASDerevyanko <ASDerevyanko@1cbit.ru>
2020-11-27 12:19:36 +0300

    Поправил синтаксис

--- bitrix/templates/.default/components/bitrix/news.list/discount.slider/result_modifier.php
+++ bitrix/templates/.default/components/bitrix/news.list/discount.slider/result_modifier.php
@@ -1,20 +1,19 @@
-<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();?>
-<!--Изменение размера изображения на лету-->
-<?
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+//Изменение размера изображения на лету
 foreach($arResult["ITEMS"] as $ID=>$arItems){
     $arImage=CFile::ResizeImageGet($arItems["DETAIL_PICTURE"], array('width'=>$arParams["LIST_PREV_PICT_W"], 'height'=>$arParams["LIST_PREV_PICT_H"]), BX_RESIZE_IMAGE_PROPORTIONAL, true);
     $arResult["ITEMS"][$ID]["DETAIL_PICTURE"]=$arImage;
 }
-?>
-<!--Вывод данных привязанного элемента через GetList-->
-<?
+
+//Вывод данных привязанного элемента через GetList
 $arTempID = array();
 $catalogIBlockID = false;
 foreach($arResult["ITEMS"] as $elem)
 {
     $arTempID[] = $elem["PROPERTIES"]["LINK_PROD"]["VALUE"];
     $catalogIBlockID = $elem["PROPERTIES"]["LINK_PROD"]["LINK_IBLOCK_ID"];
-};
+}
 $arSort = false;
 $arFilter = array(
     "IBLOCK_ID" => $catalogIBlockID,
@@ -37,5 +36,4 @@ $arResult["PROD_ELEM"] = array();
 while($arRes = $BDRes->GetNext())
 {
     $arResult["PROD_ELEM"][$arRes["ID"]] = $arRes;
-};
-?>
+}
