7b656a25c9b68000d83983aa872244f28e3e5e8c
ASDerevyanko <ASDerevyanko@1cbit.ru>
2020-11-30 11:31:28 +0300

    Попправил логику

--- bitrix/php_interface/include/event_deactivate.php
+++ bitrix/php_interface/include/event_deactivate.php
@@ -1,41 +1,25 @@
 <?
-AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", Array("CIBlockDeactivate", "OnBeforeIBlockElementUpdateDeactivate"));
+AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", "OnBeforeIBlockElementUpdateDeactivate");//Если новость создана >= 3 дней назад идеактивируется, то отменяет деактивацию и выводит сообщение
 
-    if(CModule::IncludeModule("iblock"))
+function OnBeforeIBlockElementUpdateDeactivate(&$arFields)
+{
+    if(($arFields["IBLOCK_ID"] == NEWS_IBLOCK_ID) && ($arFields["ACTIVE"] == 'N'))//Проверяет, что инфоблок == Новости и элемент неактивенна момент изменения
     {
-        $arSelect = Array("DATE_CREATE", "ACTIVE");
-        $arFilter = Array("NEWS_IBLOCK_ID", "ID"=>$arFields["ID"]);
-        $res = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>50), $arSelect);
-        while($ob = $res->GetNextElement())
+        $res = CIBlockElement::GetByID($arFields["ID"]);//получаем состояние новости до начала изменений
+        $arItems = $res->Fetch();
+
+        if($arItems["ACTIVE"] == 'Y')
         {
-            $arItems[] = $ob->GetFields();
-        }
-    }
-    
-    if($arItems[0]["ACTIVE"] == "Y")
-    {
-        class CIBlockDeactivate
-        {
-            function OnBeforeIBlockElementUpdateDeactivate(&$arFields)
+            $stmp_create = MakeTimeStamp($arItems["DATE_CREATE"], "DD.MM.YYYY HH:MI:SS");//Date create in UNIX
+            $stmp_curr = time();//Current date in UNIX
+            $stmp = ($stmp_curr - $stmp_create) / 86400;//Days have passed since the creation date
+            
+            if($stmp >= 3)//Элемент создан более 3 дней назад
             {
-                global $arItems;
-                if($arFields['IBLOCK_ID'] == NEWS_IBLOCK_ID)
-                {
-                    $date_create = $arItems[0]["DATE_CREATE"];// зададим дату создания новости
-                    $date_curr = ConvertTimeStamp();// зададим текущую дату
-                    $stmp_create = MakeTimeStamp($date_create, "DD.MM.YYYY HH:MI:SS");//Преобразуем вермя в UNIX формат
-                    $stmp_curr = MakeTimeStamp($date_curr, "DD.MM.YYYY HH:MI:SS");//Преобразуем вермя в UNIX формат
-                    //Сравнение времени в днях
-                    $curr = ($stmp_curr - $stmp_create)/(60*60*24);
-                    //При деактивации свежей новости (< 3 дней от созданий) выводить предупреждение и отменять деактивацию
-                    if(($curr <= 3) && ($arFields["ACTIVE"] == "N"))
-                    { 
-                        global $APPLICATION;
-                        $APPLICATION->throwException("Вы деактивировали свежую новость");
-                        return false;
-                    }
-                }
+                global $APPLICATION;
+                $APPLICATION->throwException("Вы деактивировали свежую новость");//Отменяем деактивацию ивыводим сообщение
+                return false;
             }
         }
     }
-?>
+}
