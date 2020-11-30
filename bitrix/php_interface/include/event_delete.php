6641d8e608fc9c49fe44b43a05eafa8ea17cd0cf
ASDerevyanko <ASDerevyanko@1cbit.ru>
2020-11-30 12:10:10 +0300

    Изменил способ получения данных

--- bitrix/php_interface/include/event_delete.php
+++ bitrix/php_interface/include/event_delete.php
@@ -1,22 +1,21 @@
 <?
 AddEventHandler("iblock", "OnBeforeIBlockElementDelete", "OnBeforeIBlockElementDeleteHandler");
-// создаем обработчик события "OnBeforeIBlockElementDelete"
+
 function OnBeforeIBlockElementDeleteHandler($ID)
 {
-    CModule::IncludeModule("iblock");
-    $arSelect = Array("ID", "SHOW_COUNTER", "NAME");
-    $arFilter = Array("IBLOCK_ID" => CATALOG_IBLOCK_ID, "ID" => $ID);
-    $res = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>50), $arSelect);
-    while($ob = $res->GetNextElement())
-    $arItems = $ob->GetFields();
-    
-    if($arItems["SHOW_COUNTER"]>1)//Если просмотров больше 1
+    $res = CIBlockElement::GetByID($ID);//Получаем данные элемента
+        $arItems = $res->Fetch();
+    if($arItems["IBLOCK_ID"] == CATALOG_IBLOCK_ID)//Если инфоблок - Продукты
     {
-        (new CIBlockElement())->Update($ID, ['ACTIVE'=>'N']);//Деактивируем элемент
-        $GLOBALS['DB']->Commit();//Сохраняем в ДБ предыдущие данные
-        global $APPLICATION;
-        $GLOBALS['APPLICATION']->throwException("Элемент нельзя удалить, у него ".$arItems["SHOW_COUNTER"]." просмотров.");
-        return false;//Отменяем удаление
+        if($arItems["SHOW_COUNTER"] > 1)//Если просмотров больше 1
+        {
+            $GLOBALS['DB']->StartTransaction();
+            (new CIBlockElement())->Update($ID, ['ACTIVE'=>'N']);//Деактивируем элемент
+            $GLOBALS['DB']->Commit();//Сохраняем в ДБ предыдущие данные
+
+            global $APPLICATION;
+            $GLOBALS['APPLICATION']->throwException("Элемент нельзя удалить, количество просмотров: ".$arItems["SHOW_COUNTER"]);
+            return false;//Отменяем удаление
+        }  
     } 
 }
-?>
