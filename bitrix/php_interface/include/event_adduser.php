adda6ff83e6fd4e9f4cf537c361f8eb7259df579
ASDerevyanko <ASDerevyanko@1cbit.ru>
2020-12-02 15:35:10 +0300

    final

--- bitrix/php_interface/include/event_adduser.php
+++ bitrix/php_interface/include/event_adduser.php
@@ -1,50 +1,40 @@
 <?
 //Подключили до попытки изменения пользователя
 AddEventHandler("main", "OnBeforeUserUpdate", "OnBeforeUserUpdateEditor");
-
-function OnBeforeUserUpdateEditor(&$arFields)
-{  
-    //Записываем в NOW_IN_GROUP был ли пользователь контент-редактором изначально
-    if(!in_array(GROUP_CONTENT_EDITOR_ID, CUser::GetUserGroup($arFields["ID"])))
-    {
-        $arFields["CONTENT_EDITOR"] = false;
-    }else{
-        $arFields["CONTENT_EDITOR"] = true;
-    } 
-}
 //Подключили после попытки изменения пользователя
 AddEventHandler("main", "OnAfterUserUpdate", "OnAfterUserUpdateEditor");
-//Добавили почтовое событие для оповещения других контент-редакторов
-AddEventHandler("main", "OnBeforeEventAdd", "OnBeforeEventAddEditor");
 
-function OnAfterUserUpdateEditor(&$arFields)
+function OnBeforeUserUpdateEditor(&$arFields)
 {
-    
-    if(!$arFields["CONTENT_EDITOR"])//Если пользователь не был контент-редактором
-    {
-        if(in_array(GROUP_CONTENT_EDITOR_ID, CUser::GetUserGroup($arFields["ID"])))//И его добавили в контент-редакторы
+    //пользователь до попытки изменения не в группе
+    if(!in_array(CONTENT_EDITOR_GROUP_ID, CUser::GetUserGroup($arFields["ID"])))
+    {   
+        global $arIDs;
+        $arIDs = CGroup::GetGroupUser(CONTENT_EDITOR_GROUP_ID);//получаем ID пользователей, состоявших в группе изначально
+        
+        function OnAfterUserUpdateEditor(&$arFields)
         {
-            //Находим всех пользователей из группы
-            $arIDs = CGroup::GetGroupUser(GROUP_CONTENT_EDITOR_ID);
-            $cnt = count($arIDs);
-            $x = 0;
-            //Формируем массив email контент-редакторов для рассылки уведомления
-            while ($x<$cnt)
+            //пользователь после попытки изменения в группе
+            if(in_array(CONTENT_EDITOR_GROUP_ID, CUser::GetUserGroup($arFields["ID"])))
             {
-                $rsUsers = CUser::GetByID($arIDs[$x]);
-                $arUsers[] = $rsUsers->Fetch();
-                $arEmail[] = $arUsers[$x]["EMAIL"];
-                $x += 1;
+                global $arIDs;
+                //Получаем email пользователей в группе
+                $strIDs = implode(",",$arIDs);
+                $arFilter = array("GROUPS_ID" => CONTENT_EDITOR_GROUP_ID, "!iD" => $arFields["ID"]);
+                $res = CUser::GetList(($by = "ID"), ($order = "asc"), $arFilter, array("FIELDS" => array("EMAIL")));
+                while($ob = $res->Fetch())
+                {
+                    $arEmails[] = $ob["EMAIL"];
+                }
+                $strEmails = implode(",", $arEmails);
+
+                //Отправляем оповещения по списку email`ов
+                $arFields = array(
+                    "EMAIL_TO" => $strEmails
+                    );
+                CEvent::Send("ADD_CONTENT_EDITOR", "s1", $arFields);
+
             }
-            //Почтовое событие
-            $arFields = array(
-                "ID"          => 32,
-                "CONTRACT_ID" => 1,
-                "TYPE_SID"    => "LEFT",
-                "EMAIL" => $arEmail
-                );
-            CEvent::Send("ADD_CONTENT_EDITOR", s1, $arFields);
         }
     }
 }
-?>
